a08a7ea4056bcd728d933aa133c798261d824891
0LucasMatheus <lucasmatheus5305@gmail.com>
2026-03-14 18:04:33 -0300

    app feat: tela home

---
+++ camus_app/lib/ui/home/widgets/logout_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LogoutButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const LogoutButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: const Text('Logout'),
+      ),
+    );
+  }
+}
