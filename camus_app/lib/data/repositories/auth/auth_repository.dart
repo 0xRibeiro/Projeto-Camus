f41354787470d29b2c4f3c27c165302048590a6b
0LucasMatheus <lucasmatheus5305@gmail.com>
2026-03-14 06:24:16 -0300

    camus_app /service inicial acabada

--- camus_app/lib/data/repositories/auth/auth_repository.dart
+++ camus_app/lib/data/repositories/auth/auth_repository.dart
@@ -1,11 +1,14 @@
 // interface de autenticacao
 
+import 'package:camus_app/domain/dtos/credentials.dart';
 import 'package:camus_app/domain/entities/user_entity.dart';
 import 'package:result_dart/result_dart.dart';
 
 abstract interface class AuthRepository {
-  AsyncResult<LoggedUser>  login();
+  AsyncResult<LoggedUser> login(Credentials credentials);
   AsyncResult<Unit> logout();
   AsyncResult<LoggedUser> getUser();
   Stream<User> userObservable();
+
+  void dispose();
 }
--- camus_app/lib/domain/validators/credentials_validator.dart
+++ camus_app/lib/domain/validators/credentials_validator.dart
@@ -1,1 +1,18 @@
 //
+
+import 'package:camus_app/domain/dtos/credentials.dart';
+import 'package:lucid_validation/lucid_validation.dart';
+
+class CredentialsValidator extends LucidValidator<Credentials> {
+  CredentialsValidator() {
+    ruleFor((c) => c.email, key: 'email').notEmpty().validEmail();
+
+    ruleFor((c) => c.password, key: 'password')
+        .notEmpty()
+        .minLength(6)
+        .mustHaveUppercase()
+        .mustHaveLowercase()
+        .mustHaveNumber()
+        .mustHaveSpecialCharacter();
+  }
+}
