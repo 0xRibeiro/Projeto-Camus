3302a7d4a79b942fc9139c273aeaa78912f0110b
0LucasMatheus <lucasmatheus5305@gmail.com>
2026-03-14 02:27:58 -0300

    camus_app /data repositories criados

--- camus_app/lib/data/repositories/auth/auth_repository.dart
+++ camus_app/lib/data/repositories/auth/auth_repository.dart
@@ -1,1 +1,11 @@
 // interface de autenticacao
+
+import 'package:camus_app/domain/entities/user_entity.dart';
+import 'package:result_dart/result_dart.dart';
+
+abstract interface class AuthRepository {
+  AsyncResult<LoggedUser>  login();
+  AsyncResult<Unit> logout();
+  AsyncResult<LoggedUser> getUser();
+  Stream<User> userObservable();
+}
--- camus_app/lib/data/repositories/auth/remote_auth_repository.dart
+++ camus_app/lib/data/repositories/auth/remote_auth_repository.dart
@@ -1,1 +1,31 @@
 // implementacao da interface de autenticacao
+
+import 'package:camus_app/data/repositories/auth/auth_repository.dart';
+import 'package:camus_app/domain/entities/user_entity.dart';
+import 'package:result_dart/result_dart.dart';
+
+class RemoteAuthRepository implements AuthRepository {
+  @override
+  AsyncResult<LoggedUser> login() {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  AsyncResult<Unit> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  AsyncResult<LoggedUser> getUser() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<User> userObservable() {
+    // TODO: implement userObservable
+    throw UnimplementedError();
+  }
+}
