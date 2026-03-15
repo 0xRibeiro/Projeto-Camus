67df52c020b5b5789e2263d4c72e89999732bbae
0LucasMatheus <lucasmatheus5305@gmail.com>
2026-03-15 03:18:09 -0300

    feat: register funcionado no app

--- camus_app/lib/data/repositories/auth/auth_repository.dart
+++ camus_app/lib/data/repositories/auth/auth_repository.dart
@@ -3,11 +3,13 @@
 import 'package:camus_app/domain/dtos/credentials.dart';
 import 'package:camus_app/domain/entities/user_entity.dart';
 import 'package:result_dart/result_dart.dart';
+import 'package:camus_app/domain/dtos/register_dto.dart';
 
 abstract interface class AuthRepository {
   AsyncResult<LoggedUser> login(Credentials credentials);
   AsyncResult<Unit> logout();
   AsyncResult<LoggedUser> getUser();
+  AsyncResult<User> register(RegisterDTO registerDTO);
   Stream<User> userObservable();
 
   void dispose();
