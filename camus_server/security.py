2974ce343745b9101f8c9e370d294f87bef69e62
MariaEduardaPaixao0 <mariaeduardapaixaodesouza71@gmail.com>
2026-04-06 10:46:14 -0300

    feat: adiciona criptografia e segurança com AES

--- camus_server/security.py
+++ camus_server/security.py
@@ -1,29 +1,43 @@
 import bcrypt
+from cryptography.fernet import Fernet
+import os
+import base64
+import hashlib
+from dotenv import load_dotenv
 
-# 1.1 Uso de hash criptográfico seguro para senhas (bcrypt)
-# 1.2 Parâmetros de custo do hash configurados e justificados
-# 2.1 Funcionalidade de recuperação de senha implementada
-# fator de custo do bcrypt (recomendado)
+# Carrega variáveis do .env
+load_dotenv()
+
+# configuração de segurança
 COST_FACTOR = 12  
 
 """
-Justificativa: O fator de custo 12 foi utilizado na configuração do algoritmo bcrypt, estabelecendo a complexidade computacional do processo de criação do hash. Esse valor foi selecionado por proporcionar um equilíbrio apropriado entre segurança e desempenho, sendo fortemente recomendado por entidades como a OWASP. O crescimento dos custos torna os ataques de força bruta consideravelmente mais desafiadores, uma vez que aumenta o tempo necessário para calcular cada tentativa de senha.
-
+Justificativa: O fator de custo 12 proporciona equilíbrio entre
+segurança e desempenho, conforme recomendado pela OWASP.
 """
+# chave de criptografia AES
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+if not SECRET_KEY:
+    raise ValueError("A SECRET_KEY não foi definida no ambiente!")
+
+def gerar_chave_fernet(secret: str) -> bytes:
+    """
+
+    Converte a chave em formato válido para o Fernet-AES.
+    """
+    hash_sha256 = hashlib.sha256(secret.encode()).digest()
+    return base64.urlsafe_b64encode(hash_sha256)
+
+CHAVE_SECRETA = gerar_chave_fernet(SECRET_KEY)
+
+fernet = Fernet(CHAVE_SECRETA)
+
+# função da senha bycript
 
 def gerar_hash_senha(senha: str) -> str:
     """
-    1.3 Uso de salt criptográfico único por usuário.
-
-    O bcrypt cria um salt aleatório para cada senha de forma automática.
-    por meio da função gensalt(). Esse salt é integrado diretamente
-    no hash final que está guardado no banco de dados.
-
-    Isso previne ataques como rainbow tables e assegura que duas senhas
-    iguais gerem hashes distintos.
-
-    1.4 Armazenamento correto do hash + salt.
-    O valor retornado já contém o hash e o salt no mesmo campo.
+    Gera hash seguro da senha usando bcrypt.
     """
     salt = bcrypt.gensalt(rounds=COST_FACTOR)
     senha_hash = bcrypt.hashpw(senha.encode("utf-8"), salt)
@@ -32,12 +46,24 @@ def gerar_hash_senha(senha: str) -> str:
 
 def verificar_senha(senha: str, senha_hash: str) -> bool:
     """
-    A função bcrypt.checkpw() é utilizada para comparar a senha fornecida
-    pelo usuário com o hash armazenado no banco de dados. O bcrypt extrai
-    automaticamente o salt do hash armazenado e o utiliza para calcular o hash
-    da senha fornecida, garantindo uma comparação segura e eficiente.
+    Verifica se a senha corresponde ao hash.
     """
     return bcrypt.checkpw(
         senha.encode("utf-8"),
         senha_hash.encode("utf-8")
     )
+
+
+
+
+# função da criptogranfia AES
+def criptografar_dado(dado: str) -> str:
+    """ Criptografa dados sensíveis utilizando AES.
+    """
+    return fernet.encrypt(dado.encode()).decode()
+
+
+def descriptografar_dado(dado_criptografado: str) -> str:
+    """Descriptografa dados previamente criptografados
+    """
+    return fernet.decrypt(dado_criptografado.encode()).decode()
