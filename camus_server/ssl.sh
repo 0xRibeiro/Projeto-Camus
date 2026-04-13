7bf671aa25a7775d84f802a2c09ee71ac6e853c4
0xRibeiro <142441056+0xRibeiro@users.noreply.github.com>
2026-04-13 00:22:21 -0300

    docs: explica o uso temporario do script ssl.sh

--- camus_server/ssl.sh
+++ camus_server/ssl.sh
@@ -1,3 +1,5 @@
+## Script para a geração de certificados SSL auto-assinados, utilizando o openssl.
+## Futuramente será necessário aperfeiçoar a criação dos certifcados.
 
 openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 \
   -subj "/C=BR/ST=SP/L=Mogi/O=Camus/OU=Camus Main Group/CN=localhost"
